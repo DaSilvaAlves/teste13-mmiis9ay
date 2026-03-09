d331c20184079f3022e02beb7ae73d118b5996bb
Eurico Alves <euricojsalves@gmail.com>
2026-03-09 01:46:18 +0000

    feat: add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// removed local import
+// removed local import
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
